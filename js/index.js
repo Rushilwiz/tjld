65201aaa9dff8d6d3d10f69f7c8da2ac88f40a61
Rushil Umaretiya <rushilwiz@gmail.com>
2021-08-16 03:36:27 +0200

    feat: added landing and mobile for nav

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var options = {
+  strings: [
+    "every Friday 8A",
+    "at the #1 highschool in the nation",
+    "taught by TJ's brightest",
+  ],
+  typeSpeed: 40,
+  backSpeed: 20,
+  startDelay: 1000,
+  backDelay: 5000,
+  loop: true,
+  shuffle: true,
+};
+
+var typed = new Typed("#hero-subtitle", options);
